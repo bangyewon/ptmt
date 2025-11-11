9eca9bb7544d96648864818fd0a35a6951ccfcd7
bangyewon <byewon1111@gmail.com>
2025-11-11 14:45:23 +0900

    [feat] ipset에 ip추가 로직 추가

---
+++ ipset.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int addIp(const char *ip) {
+	char cmd[256];
+	snprintf(cmd,sizeof(cmd), "ipset add whiteist %s",ip);
+	int success = system(cmd);
+	printf("%s\n",ip); //디버깅용
+	return (success == 0);
+
+}
+
+char *trim(char *str) {
+	char *end;
+
+	while(isspace(*str)) {
+		str++;
+	}
+	if (*str) {
+		end = str + strlen(str) - 1;
+		while(end > str && isspace(*end)) {
+		
+			end--;
+		}
+		*(end + 1) = '\0';
+	}
+	return str;
+
+}
+
+int registerIp(const char *filename) {
+	FILE *fp = fopen(filename,"r");
+
+	char line[1024];
+	int lineNu = 1;
+	if(fp == NULL) {
+		printf("파일을 열 수 없어요.\n");
+		return 1;
+	}
+
+	while(fgets(line,sizeof(line),fp)) {
+	// 두번째줄부터 읽기 시작해서 공백 다 제거 후 ip만 가져오기
+		char *trimmed = trim(line);
+		if(strlen(trimmed) > 0) {
+			printf("%s\n",trimmed);
+			}
+		addIp(line);
+		}
+	printf("ip등록 완료"); //디버깅용
+	
+	fclose(fp);
+	return 0;
+
+}
---
+++ ipset.h
@@ -0,0 +1,7 @@
+#ifdef IPSET_H
+#define IPSET_H
+#include <stdbool.h>
+
+int registerIp(const char *filename);
+
+#endif
